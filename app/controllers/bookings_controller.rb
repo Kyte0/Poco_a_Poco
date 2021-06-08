d6eba6661367e01f96c77f65aff372f138bd2e99
Tiana Milanovich <tianarose.m@outlook.com>
2021-06-08 16:59:38 +0100

    mistake was made

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -39,6 +39,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, "")
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
