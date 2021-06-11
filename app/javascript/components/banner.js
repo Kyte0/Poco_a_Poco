3f71c27ec83c49ef1a000d70ff63bf98019d92ce
Laurence Sainsbury <laurencesainsbury@gmail.com>
2021-06-11 10:41:00 +0100

    change the banner text on the homepage, and add gradient to show page

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,9 +1,19 @@
 import Typed from 'typed.js';
 
+const quotes = [
+  "If music be the food of love, play on.",
+  "Music can name the unnameable and communicate the unknowable.",
+  "Everything in the universe has a rhythm, everything dances.",
+  "Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.",
+  "The music is not in the notes, but in the silence in between.",
+  "Music, once admitted to the soul, becomes a sort of spirit and never dies.",
+  "Beans, beans, the musical fruit, the more you eat, the more you toot."
+]
+
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to play some tunes"],
-    typeSpeed: 50,
+    strings: quotes,
+    typeSpeed: 60,
     loop: true
   });
 }
